ea595855089c174d3f01de6b8ad98d66619671f8
MarcosGruah <110102063+MarcosGruah@users.noreply.github.com>
2023-09-04 16:55:37 -0300

    Feature: Marking tasks as completed.

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int taskCount = 1;
+        private int id = 1;
 
         public MainWindow()
         {
@@ -32,11 +32,31 @@ namespace ToDoApp
             string todoText = TodoInput.Text;
             if (!string.IsNullOrEmpty(todoText))
             {
-                TextBlock todoItem = new TextBlock { Text = $"{taskCount} - {todoText}" };
-                TodoList.Children.Add(todoItem);
+                CheckBox todoItemCheckbox = new CheckBox { Name = $"itemCheckboxID_{id}", Margin = new Thickness(2.5) };
+                TextBlock todoItemText = new TextBlock { Name = $"itemTextID_{id}", Text = $"{todoText}", TextWrapping = TextWrapping.Wrap };
+                todoItemCheckbox.Checked += new RoutedEventHandler(CheckboxChecked);
+                todoItemCheckbox.Unchecked += new RoutedEventHandler(CheckboxUnchecked);
+                todoItemCheckbox.Content = todoItemText;
+                TodoList.Children.Add(todoItemCheckbox);
                 TodoInput.Clear();
-                taskCount++;
+                id++;
             }
+        }
+
+        private void CheckboxChecked(object sender, EventArgs e)
+        {
+            CheckBox checkbox = (CheckBox)sender;
+            TextBlock itemText = (TextBlock)checkbox.Content;
+            itemText.TextDecorations = TextDecorations.Strikethrough;
+            itemText.Opacity = 0.5;
+        }
+
+        private void CheckboxUnchecked(object sender, EventArgs e)
+        {
+            CheckBox checkbox = (CheckBox)sender;
+            TextBlock itemText = (TextBlock)checkbox.Content;
+            itemText.TextDecorations = null;
+            itemText.Opacity = 1;
         }
     }
 }
